729b224a2cbf258ee8c0d4283592fa00eda3fe31
unknown <imad.elamrani.solicode@gmail.com>
2021-12-14 15:28:38 +0100

    +++

--- form.js
+++ form.js
@@ -18,6 +18,7 @@ function validation(){
     let type=document.getElementsByName("type");
     let label=document.getElementsByClassName("valid");
     var validation=0;
+    var table = document.getElementsByTagName("table")[0];
     
     
 
@@ -32,7 +33,6 @@ function validation(){
     
 
     if (type[0].checked || type[1].checked || type[2].checked || type[3].checked){
-      i=1;
       document.getElementById("typelabel").innerHTML="champ validé";
       document.getElementById("typelabel").style.color = "rgb(23, 214, 39)";
       validation++;
@@ -42,7 +42,6 @@ function validation(){
     }
 
     if(titre.length>0 && titre.length<=30){
-      i=2;
       document.getElementById("labeltitre").innerHTML="champ validé";
       document.getElementById("labeltitre").style.color = "rgb(23, 214, 39)";
       validation++;
@@ -56,7 +55,6 @@ function validation(){
     
 
     if(auteur.length<=30 && auteur.length>0){
-      i=3;
       document.getElementById("labelauteur").innerHTML="champ validé";
       document.getElementById("labelauteur").style.color = "rgb(23, 214, 39)";
       validation++;
@@ -71,7 +69,6 @@ function validation(){
      
 
     if(prix>0){
-      i=4;
       document.getElementById("labelprix").innerHTML="champ validé";
       document.getElementById("labelprix").style.color = "rgb(23, 214, 39)";
       validation++;
@@ -89,7 +86,6 @@ function validation(){
       document.getElementById("labelangue").style.color = "red";
     }
     else{
-      i=5;
       document.getElementById("labelangue").innerHTML="champ validé";
       document.getElementById("labelangue").style.color = "rgb(23, 214, 39)";
       validation++;
@@ -103,7 +99,6 @@ function validation(){
       validation++;
     }
     if(validation==6){
-        
             var row = table.insertRow(-1);
             row.insertCell(0).innerHTML = titre;
             row.insertCell(1).innerHTML = auteur;
@@ -115,23 +110,60 @@ function validation(){
                 row.insertCell(5).innerHTML = type[i].value;
               }
             }
-            row.insertCell(6).innerHTML = `<button onClick="onEdit(this)">MODIFIER</button>
-                                           <button onClick="onDelete(this)">SUPPRIMER</button>`;
+            row.insertCell(6).innerHTML = `<button onClick="modifier(this)">MODIFIER</button>
+                                           <button onClick="supprimer(this)">SUPPRIMER</button>`;
           }
      
 
 }
-function onEdit(td) {
+function modifier(td) {
   selectedRow = td.parentElement.parentElement;
     document.getElementById("titre").value = selectedRow.cells[0].innerHTML;
     document.getElementById("auteur").value = selectedRow.cells[1].innerHTML;
     document.getElementById("prix").value = selectedRow.cells[2].innerHTML;
     document.getElementById("langue").value = selectedRow.cells[3].innerHTML;
     document.getElementById("date").value = selectedRow.cells[4].innerHTML;
-    document.getElementsByName("type").value = selectedRow.cells[5].innerHTML;
+    for(i=0;i<=3;i++){
+      if(type[i].value == selectedRow.cells[5].innerHTML){
+        type[i].checked = type[i].value ;
+      }
+    }
+
+
+    function  UpdateBtn(){
+
+      
+      // var checkedValue;
+      selectedRow.cells[0].innerHTML =  titre.value;
+      selectedRow.cells[1].innerHTML = auteur.value;
+      selectedRow.cells[2].innerHTML = prix.value;
+      selectedRow.cells[3].innerHTML =  date.value;
+      selectedRow.cells[4].innerHTML =  langue.value;
+      
+      // for(var radios of type){
+      //     if(radios.checked)
+      //     {
+      //       checkedValue = radios.value;
+      //     }
+      //   }
+      
+      //   selectedRow.cells[5].innerHTML=checkedValue;
+      
 }
-function onDelete(td) {
+
+
+
+
+function supprimer(r){
+  var j=r.parentNode.parentNode.rowIndex;
+  table.deleteRow(j);
+}
+
+
+
+/*function onDelete(td) {
   if (confirm('Are you sure to delete this record ?')) {
     document.getElementsByTagName("input").value ="";
   }
+*/
 }
